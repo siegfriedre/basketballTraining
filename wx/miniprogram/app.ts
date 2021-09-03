ed1c317dab31a73a1de9944a22e0c2a8536bfc9c
siegfriedre <siegfriedzzy@163.com>
2021-09-03 19:16:43 +0800

    update app.ts index.ts

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,6 +1,33 @@
+import {  getUserProfile } from "./utils/util"
+
+let resolveUserInfo : (value: WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>) => void
+let rejectUserInfo : (reason?: any) => void
+
 // app.ts
 App<IAppOption>({
-  globalData: {},
+  globalData: {
+    userInfo: new Promise((resolve,reject) => {
+      // 获取用户信息 - Promise版
+      // getsetting可去？
+      // getSetting().then(res => {
+      //   if(res.authSetting['scope.userInfo']){
+      //     return getUserProfile()
+      //   }
+      //   return undefined
+      // })
+      console.log('只调用一次')
+      // getUserProfile().then(res => {
+      //   if(!res){
+      //     return 
+      //   }
+      //   // 通知外面我获得了用户信息
+      //   resolve(res.userInfo)
+      // }).catch(err => reject(err))
+      // 存储两个函数
+      resolveUserInfo = resolve
+      rejectUserInfo = reject
+    }),
+  },
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
@@ -14,6 +41,11 @@ App<IAppOption>({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       },
     })
-    
   },
+
+  //暴露给外面，让其他页面也可以调用该函数，最好不用export
+  //需要在index.d.ts声明
+  resolveUserInfo(userInfo : WechatMiniprogram.UserInfo) :void {
+    resolveUserInfo(userInfo) // 虽然重名，但没加this就调全局变量
+  }
 })
--- wx/miniprogram/utils/util.ts
+++ wx/miniprogram/utils/util.ts
@@ -17,3 +17,23 @@ const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
 }
+
+//没用
+export function getSetting() : Promise<WechatMiniprogram.GetSettingSuccessCallbackResult>{
+  return new Promise((resolve,reject) => {
+    wx.getSetting({
+      success: res => resolve(res),
+      fail: err => reject(err),
+    })
+  })
+}
+
+export function getUserProfile() : Promise<WechatMiniprogram.GetUserProfileSuccessCallbackResult>{
+  return new Promise((resolve,) => {
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => resolve(res),
+      // fail: err => reject(err),
+    })
+  })
+}
